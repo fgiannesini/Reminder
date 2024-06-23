cfb9832d8a4daf32050b554009679d0d5f7f385f
Fabien <fabien.giannesini@gmail.com>
2024-06-23 20:42:22 +0200

    Method to enhance inputs

--- src/test/java/com/fgiannesini/ReminderTest.java
+++ src/test/java/com/fgiannesini/ReminderTest.java
@@ -12,17 +12,26 @@ class ReminderTest {
     @Test
     public void Should_validate_a_translation_from_portugues_to_french() throws IOException {
         OutputStream outputStream = Mockito.mock(OutputStream.class);
-        Reminder reminder = new Reminder(new ByteArrayInputStream("éteindre\r\nquit".getBytes()), outputStream);
+        Reminder reminder = new Reminder(getInputStream("""
+                éteindre
+                quit"""), outputStream);
         reminder.run();
         Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
         Mockito.verify(outputStream).write("desligar\r\n".getBytes());
         Mockito.verify(outputStream).write("OK\r\n".getBytes());
     }
 
+    private static ByteArrayInputStream getInputStream(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
     @Test
     public void Should_validate_two_translations_from_portugues_to_french() throws IOException {
         OutputStream outputStream = Mockito.mock(OutputStream.class);
-        Reminder reminder = new Reminder(new ByteArrayInputStream("éteindre\r\nallumer\r\nquit".getBytes()), outputStream);
+        Reminder reminder = new Reminder(getInputStream("""
+                éteindre
+                allumer
+                quit"""), outputStream);
         reminder.run();
         Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
         Mockito.verify(outputStream).write("desligar\r\n".getBytes());
@@ -33,7 +42,9 @@ class ReminderTest {
     @Test
     public void Should_reject_a_translation_from_portugues_to_french() throws IOException {
         OutputStream outputStream = Mockito.mock(OutputStream.class);
-        Reminder reminder = new Reminder(new ByteArrayInputStream("allumer\r\nquit".getBytes()), outputStream);
+        Reminder reminder = new Reminder(getInputStream("""
+                allumer
+                quit"""), outputStream);
         reminder.run();
         Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
         Mockito.verify(outputStream).write("desligar\r\n".getBytes());
@@ -43,7 +54,7 @@ class ReminderTest {
     @Test
     public void Should_quit_if_asked() throws IOException {
         OutputStream outputStream = Mockito.mock(OutputStream.class);
-        Reminder reminder = new Reminder(new ByteArrayInputStream("quit".getBytes()), outputStream);
+        Reminder reminder = new Reminder(getInputStream("quit"), outputStream);
         reminder.run();
         Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
         Mockito.verify(outputStream).write("desligar\r\n".getBytes());
