8f4ccb0c44ed03ac74dd3e78949d0fb743eaee0e
Fabien Giannesini <fabien.giannesini@gmail.com>
2024-06-23 19:38:58 +0200

    Should_validate_a_translation_from_portugues_to_french Should_reject_a_translation_from_portugues_to_french

--- src/main/java/com/fgiannesini/Reminder.java
+++ src/main/java/com/fgiannesini/Reminder.java
@@ -1,4 +1,19 @@
 package com.fgiannesini;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Scanner;
+
 public class Reminder {
+    public Reminder(InputStream inputStream, OutputStream outputStream) throws IOException {
+        outputStream.write(("Reminder" + System.lineSeparator()).getBytes());
+        outputStream.write(("desligar" + System.lineSeparator()).getBytes());
+        String s = new Scanner(inputStream).nextLine();
+        if (s.equals("éteindre")) {
+            outputStream.write(("OK" + System.lineSeparator()).getBytes());
+        } else {
+            outputStream.write(("KO (éteindre)" + System.lineSeparator()).getBytes());
+        }
+    }
 }
--- src/test/java/com/fgiannesini/ReminderTest.java
+++ src/test/java/com/fgiannesini/ReminderTest.java
@@ -1,12 +1,29 @@
 package com.fgiannesini;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 class ReminderTest {
 
     @Test
-    public void do_something () {
-        Assertions.assertTrue(true);
+    public void Should_validate_a_translation_from_portugues_to_french() throws IOException {
+        OutputStream outputStream = Mockito.mock(OutputStream.class);
+        new Reminder(new ByteArrayInputStream("éteindre".getBytes()), outputStream);
+        Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
+        Mockito.verify(outputStream).write("desligar\r\n".getBytes());
+        Mockito.verify(outputStream).write("OK\r\n".getBytes());
+    }
+
+    @Test
+    public void Should_reject_a_translation_from_portugues_to_french() throws IOException {
+        OutputStream outputStream = Mockito.mock(OutputStream.class);
+        Reminder reminder = new Reminder(new ByteArrayInputStream("allumer".getBytes()), outputStream);
+        Mockito.verify(outputStream).write("Reminder\r\n".getBytes());
+        Mockito.verify(outputStream).write("desligar\r\n".getBytes());
+        Mockito.verify(outputStream).write("KO (éteindre)\r\n".getBytes());
     }
 }
