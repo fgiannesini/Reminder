65190b4cea5e44576f1555a8b4cd97c648148097
Fabien <fabien.giannesini@gmail.com>
2024-06-29 22:37:20 +0200

    Store a copy of the file

--- src/main/java/com/fgiannesini/FileStorageHandler.java
+++ src/main/java/com/fgiannesini/FileStorageHandler.java
@@ -1,24 +1,65 @@
 package com.fgiannesini;
 
+import com.opencsv.CSVWriter;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import com.opencsv.exceptions.CsvException;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
 public class FileStorageHandler implements StorageHandler {
 
-    public List<Word> load(Path csvFilePath) throws IOException {
+    private final Path storageDir;
+
+    public FileStorageHandler(Path storageDir) {
+        this.storageDir = storageDir;
+    }
+
+    private static List<CsvOriginalWord> readCsvFile(Path csvFilePath) throws IOException {
         try (Reader reader = Files.newBufferedReader(csvFilePath)) {
             CsvToBean<CsvOriginalWord> cb = new CsvToBeanBuilder<CsvOriginalWord>(reader)
                     .withType(CsvOriginalWord.class)
                     .withSeparator(';')
                     .build();
-            List<CsvOriginalWord> csvOriginalWords = cb.parse();
-            return csvOriginalWords.stream().map(CsvOriginalWord::toWord).toList();
+            return cb.parse();
+        }
+    }
+
+    public List<Word> load(Path csvFilePath) throws IOException {
+        var words = readCsvFile(csvFilePath)
+                .stream()
+                .map(CsvOriginalWord::toWord)
+                .toList();
+
+        writeCsvFile(words, storageDir.resolve(csvFilePath.getFileName()));
+        return words;
+    }
+
+    private void writeCsvFile(List<Word> words, Path filePath) throws IOException {
+        var csvWords = words.stream().map(w -> {
+            CsvWord csvWord = new CsvWord();
+            csvWord.setWord(w.word());
+            csvWord.setTranslation(w.translation());
+            return csvWord;
+        }).toList();
+
+        try (Writer writer = new FileWriter(filePath.toFile(), StandardCharsets.UTF_8)) {
+            StatefulBeanToCsv<CsvWord> sbc = new StatefulBeanToCsvBuilder<CsvWord>(writer)
+                    .withSeparator(';')
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .build();
+            sbc.write(csvWords);
+        } catch (CsvException e) {
+            throw new IOException(e);
         }
     }
 }
--- src/test/java/com/fgiannesini/FileStorageHandlerTest.java
+++ src/test/java/com/fgiannesini/FileStorageHandlerTest.java
@@ -2,19 +2,23 @@ package com.fgiannesini;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 class FileStorageHandlerTest {
 
     @Test
-    void should_load_from_file() throws URISyntaxException, IOException {
+    void should_load_from_file(@TempDir Path tempDir) throws URISyntaxException, IOException {
         var path = Paths.get(ClassLoader.getSystemResource("dictionary-for-test.csv").toURI());
 
-        var storageHandler = new FileStorageHandler();
+        var storageHandler = new FileStorageHandler(tempDir);
         List<Word> wordList = storageHandler.load(path);
 
         var expected = List.of(
@@ -23,6 +27,15 @@ class FileStorageHandlerTest {
         );
 
         Assertions.assertEquals(expected, wordList);
+        File[] files = tempDir.toFile().listFiles();
+        Assertions.assertNotNull(files);
+        Assertions.assertEquals(1, files.length);
+        File file = files[0];
+        List<String> elements = Files.readAllLines(file.toPath());
+        String actual = String.join("\n", elements);
+        Assertions.assertEquals(actual, """
+                ao inves, em vez de;au lieu de
+                ou seja;c'est à dire""");
     }
 
 }
