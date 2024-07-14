6e52185bba39d8a581fbfcb98cba24dcd8be3ae1
Fabien <fabien.giannesini@gmail.com>
2024-07-14 15:16:44 +0200

    generate installer

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("org.panteleyev.jpackageplugin") version "1.6.0"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.fgiannesini"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -11,20 +12,45 @@ repositories {
 
 dependencies {
     implementation("com.opencsv:opencsv:5.9")
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.mockito:mockito-core:5.12.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 tasks {
+    test {
+        useJUnitPlatform()
+    }
     compileJava {
         options.encoding = "UTF-8"
     }
     compileTestJava {
         options.encoding = "UTF-8"
+    }
+}
+
+task("copyDependencies", Copy::class) {
+    from(configurations.runtimeClasspath).into("$buildDir/jars")
+}
+
+task("copyJar", Copy::class) {
+    from(tasks.jar).into("$buildDir/jars")
+}
+
+tasks.jpackage {
+    dependsOn("build", "copyDependencies", "copyJar")
+
+    input = "$buildDir/jars"
+    destination = "$buildDir/dist"
+
+    appName = "Reminder"
+    vendor = "fgiannesini"
+
+    mainJar = tasks.jar.get().archiveFileName.get()
+    mainClass = "com.fgiannesini.Main"
+
+    javaOptions = listOf("-Dfile.encoding=UTF-8")
+
+    windows {
+        winConsole = true
     }
 }
