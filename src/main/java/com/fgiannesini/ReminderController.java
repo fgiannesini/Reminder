df0f674f43b7529e9e36c55fb95df83eb253dae6
Fabien Giannesini <fabien.giannesini@gmail.com>
2024-10-06 15:19:14 +0200

    Rename ApiController to ReminderController

---
+++ src/main/java/com/fgiannesini/ReminderController.java
@@ -0,0 +1,18 @@
+package com.fgiannesini;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reminder/word")
+public class ReminderController {
+
+    @GetMapping("/next")
+    public String next() {
+        return "Hello, World!";
+    }
+
+    @PostMapping("/check")
+    String check(@RequestParam String data) {
+        return "Data received: ${data.name}";
+    }
+}
