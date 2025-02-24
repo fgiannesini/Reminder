db02566163d7ea9332b5e61d320120773cdaf1f2
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-02-24 12:54:01 +0100

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.4.2"
+    id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.testcontainers:testcontainers-bom:1.20.4"))
+    implementation(platform("org.testcontainers:testcontainers-bom:1.20.5"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
