81a136577a8df19b76c1c8fb2e9f2481611a7622
Fabien Giannesini <fabien.giannesini@gmail.com>
2025-04-25 13:08:07 +0200

    Detect duplicates

--- src/main/java/com/fgiannesini/console/Main.java
+++ src/main/java/com/fgiannesini/console/Main.java
@@ -1,6 +1,33 @@
 package com.fgiannesini.console;
 
+import com.fgiannesini.Word;
+import com.fgiannesini.original.OriginalDictionary;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        var originalFileInputStream = ClassLoader.getSystemResourceAsStream("dictionary.csv");
+        List<Word> load = new OriginalDictionary(originalFileInputStream).load();
+        for (int i = 0; i < load.size() - 1; i++) {
+            Word word = load.get(i);
+            for (int j = i + 1; j < load.size(); j++) {
+                Word word2 = load.get(j);
+                boolean wordToLearn = Arrays.stream(getSplit(word.wordToLearn())).anyMatch(s -> Arrays.asList(getSplit(word2.wordToLearn())).contains(s));
+                if (wordToLearn) {
+                    System.out.println(word.wordToLearn() + " " + i + " " + word2.wordToLearn() + " " + j);
+                }
+                boolean translation = Arrays.stream(getSplit(word.translation())).anyMatch(s -> Arrays.asList(getSplit(word2.translation())).contains(s));
+                if (translation) {
+                    System.out.println(word.translation() + " " + i + " " + word2.translation() + " " + j);
+                }
+            }
+        }
+    }
+
+    private static String[] getSplit(String word) {
+        return word.split(";")[0].split(",");
     }
 }
