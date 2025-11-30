1f54b31d017e229ce1965789de37dbd0d2b8bc3b
Fabien <fabien.giannesini@gmail.com>
2025-11-30 21:54:01 +0100

    Add spring web

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-restclient")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.opencsv:opencsv:5.12.0")
